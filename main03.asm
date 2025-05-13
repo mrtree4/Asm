ac196f91556d054e7cc0d178202ec4690a87e555
mrtree4 <60297404+mrtree4@users.noreply.github.com>
2025-05-13 12:20:05 +0300

    Рефакторинг 3-ей задачи

--- main03.asm
+++ main03.asm
@@ -1,29 +1,33 @@
 .386
 
 dseg segment use16
-    x db 20d
+    x db ?
     y dw ?
 dseg ends
+
 cseg segment use16
 assume ds:dseg, cs:cseg
-m1:
-    mov cx, dseg
-    mov ds, cx
-    cmp ds:x, 30
-    jb short f1
-    je short f2
-    mov ax, 84d
-    sub ax, word ptr ds:x
-    jmp short fin
-f1: mov ax, 120d
-    sub ax, word ptr ds:x
-    jmp short fin
-f2: mov ax, 140d
-    add ax, word ptr ds:x
-fin:
-    mov ds:y, ax
-    mov ah, 4ch
-    int 21h
+m1:  mov cx, dseg
+     mov ds, cx
+
+     cmp ds:x, 30
+     jb short f1
+     je short f2
+     mov ax, 84d
+     sub ax, word ptr ds:x ; -x + 84
+     jmp short fin
+
+f1:  mov ax, 120d
+     sub ax, word ptr ds:x ; 120 - x
+     jmp short fin
+
+f2:  mov ax, 140d
+     add ax, word ptr ds:x ; 140 + x
+
+fin: mov ds:y, ax
+
+     mov ah, 4ch
+     int 21h
 cseg ends
 end m1
     
