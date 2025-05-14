3ea6f124ab8ac89030557d84a1ffa9a4c10ef8ea
rachhhen <147819969+rachhhen@users.noreply.github.com>
2025-05-14 20:29:41 +0300

    Add files via upload

--- main02.asm
+++ main02.asm
@@ -5,43 +5,43 @@ dseg segment use16
     numb2 dw 07F2Dh   
     result dd ?  
     change dd ?    
-    code   dw ?
+    code dw ?
 dseg ends
-
 cseg segment use16
 assume ds:dseg, cs:cseg
 m1: mov cx, dseg
     mov ds, cx
     movzx ax, ds:numb1
     mul ds:numb2
-    ; сохранение в result
-    mov bl, al ;мл. байт
-    mov bh, dh ;ст. байт
-    mov ds:res2, ebx
-    rol bx, 8
+    mov bh, dh
+    mov bl, al
     shl edx, 16
     mov dx, ax
-    mov ds:res1, edx 
-    mov cx, ds:numb2
-    bt cx, 12
-    rcl dx, 1
-    bt cx, 11
-    rcl dx, 1
-    bt cx, 10
-    rcl dx, 1
-    bt cx, 9
-    rcl dx, 1
-    bt cx, 8
-    rcl dx, 1
-    bt cx, 7
-    rcl dx, 1
-    bt cx, 6
-    rcl dx, 1
-    shl dx, 6
+    mov ds:result, edx
+    mov dh, bh
+    mov dl, bl
+    and bh, 00001111b
+    and bl, 11110000b
+    or bh, bl
+    or bl, dl
+    and dh, 11110000b
+    and bl, 00001111b
+    or bl, dh
+    mov eax, ds:result
+    and eax, 00001111111111111111111100001111b
+    mov cl, bl 
+    and bl, 00000000b
+    shl ebx, 16
+    or eax, ebx
+    or eax, ecx
+    mov ds:change, eax
+    and dx, 0000000000000000b
     or dx, 1110000000000000b
-    and dx, 111111111000000b
-    mov ds:res3, dx
+    or dx, ds:numb2   
+    and dx, 1111111111000000b
+    mov ds:code, dx
     mov ah, 4ch
     int 21h
 cseg ends
 end m1
+    
